35cddf2e2f61c042709cd1b5af92dc58167f938d
Luis Caceres <lacc97@protonmail.ch>
2021-05-23 21:49:30 +0100

    Extract out some calculation from main calculation loop

--- xrd_simulation/diffraction.cpp
+++ xrd_simulation/diffraction.cpp
@@ -1,9 +1,17 @@
 #include "diffraction.hpp"
 
+#include <gslpp/integration.hpp>
 #include <gslpp/spline.hpp>
 
 #include <constants.hpp>
 #include <math.hpp>
+
+namespace {
+  real_t temp_dimmensionless_phi(real_t x) {
+    real_t d = gsl::integration::qag_adaptive([x](double e) -> double { return e / (std::exp(e) - 1); }, 0, x, gsl::integration::rule::e_Gauss_41, 0, 1e-5);
+    return d / x;
+  }
+}    // namespace
 
 void xrd::single_plane_diffraction_pattern::generate(const rdata_t& angles, rdata_t& intensities) const {
   rmatrix_t<3, Eigen::Dynamic> delta_k_vectors;
@@ -16,22 +24,23 @@ void xrd::single_plane_diffraction_pattern::generate(const rdata_t& angles, rdat
   const real_t tan_s2 = std::tan(m_ReceivingSollerSlitAngle);
 
   real_t debye = m_Crystal.debye_temperature();
-  auto fn_diff_intensity = [&, x = debye / m_Temperature](real_t theta) noexcept -> real_t {
+  const real_t x = debye / m_Temperature;
+  auto fn_diff_intensity = [&, x, x_2 = x*x, phi_x = temp_dimmensionless_phi(x)](real_t theta) noexcept -> real_t {
     const real_t sin_theta = std::sin(theta);
     const real_t csc_theta = 1 / sin_theta;
     const real_t cos_theta = std::cos(theta);
-    const real_t sin_2theta = 2*sin_theta*cos_theta;
+    const real_t sin_2theta = 2 * sin_theta * cos_theta;
     const real_t cos_2theta = std::cos(2 * theta);
 
-    auto fn_debye_waller = [this, debye, x, x_2 = x * x, sin_theta](const xrd::basis::atom& atom) noexcept -> real_t {
+    auto fn_debye_waller = [this, debye, x, x_2, phi_x, sin_theta](const xrd::basis::atom& atom) noexcept -> real_t {
       /* This is only correct for monatomic cubic crystals */
       constexpr real_t C1 = 1.915045e3;    // h^2 / k_b considering units of length are angstroms, units of mass are daltons
 
-      const real_t C2 = 1 + (x_2 / 36) - (x_2 * x_2 / 3600);    // Taylor expansion around 0 of [phi(x) + x/4]
-      return std::exp(-2 * ((3 * C1) / (2 * C_PI * C_PI * atom.m * x * debye)) * C2 * 2 * C_PI * sin_theta * sin_theta / (m_XrayWavelength*m_XrayWavelength));
+      const real_t C2 = phi_x + x/4;    // Taylor expansion around 0 of [phi(x) + x/4]
+      return std::exp(-2 * ((3 * C1) / (2 * C_PI * C_PI * atom.m * x * debye)) * C2 * 2 * C_PI * sin_theta * sin_theta / (m_XrayWavelength * m_XrayWavelength));
     };
 
-    const real_t f_lorentz = 2*powder_grain_distribution_factor(std::asin(tan_s2/sin_2theta), m_MosaicSpread)/sin_2theta;
+    const real_t f_lorentz = 1 / (2 * sin_theta * sin_2theta);
     const real_t f_polarization = (1 + cos_2theta * cos_2theta) / 2;
 
     real_t intensity = 0;
@@ -79,7 +88,7 @@ rmatrix_t<3, n_dynamic> xrd::single_plane_diffraction_pattern::generate_random_p
 }
 
 real_t xrd::powder_grain_distribution_factor(real_t a, real_t sigma) noexcept {
-//  return 0.5;
-//  return a;
-  return math::erf(a/(C_SQRT2*sigma));
+  //  return 0.5;
+  return a;
+  //  return math::erf(a/(C_SQRT2*sigma));
 }
